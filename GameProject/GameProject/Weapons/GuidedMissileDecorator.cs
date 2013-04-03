4445012dc184902bd22da5508e4a35ff686ea7af
Jeremy Granadillo <akakitsu@gmail.com>
2013-04-03 11:45:22 +0200

    Add basic variables

--- GameProject/GameProject/Weapons/GuidedMissileDecorator.cs
+++ GameProject/GameProject/Weapons/GuidedMissileDecorator.cs
@@ -8,6 +8,17 @@ namespace GameProject
 {
     public class GuidedMissileDecorator : WeaponUpgrade
     {
+        private Vector2 _offset = new Vector2(0, -25);
+        private Vector2 _speed = new Vector2(0, -10);
+
+        private static int _pelletNum = 5;
+
+        private static float _totalDamage = 2.0f;
+        private static float _pelletDamage = _totalDamage / _pelletNum;
+
+        private static int _lastFired;
+        private static int _shootTimer = 2000;
+
         public GuidedMissileDecorator(IWeapon wpn) {
             _wpn = wpn;
         }
--- GameProject/GameProject/Weapons/RocketDecorator.cs
+++ GameProject/GameProject/Weapons/RocketDecorator.cs
@@ -8,6 +8,17 @@ namespace GameProject
 {
     public class RocketDecorator : WeaponUpgrade
     {
+        private Vector2 _offset = new Vector2(0, -25);
+        private Vector2 _speed = new Vector2(0, -10);
+
+        private static int _pelletNum = 2;
+
+        private static float _totalDamage = 3.0f;
+        private static float _pelletDamage = _totalDamage / _pelletNum;
+
+        private static int _lastFired;
+        private static int _shootTimer = 1000;
+
         public RocketDecorator(IWeapon wpn) {
             _wpn = wpn;
         }
