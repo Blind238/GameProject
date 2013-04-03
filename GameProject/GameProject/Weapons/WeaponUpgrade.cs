6957e72d8265964c29df989e9ccf79561ed249ab
Jeremy Granadillo <akakitsu@gmail.com>
2013-04-03 11:44:01 +0200

    Missed this gamtime => gameTime

--- GameProject/GameProject/Weapons/WeaponUpgrade.cs
+++ GameProject/GameProject/Weapons/WeaponUpgrade.cs
@@ -9,6 +9,6 @@ namespace GameProject
     public abstract class WeaponUpgrade : IWeapon
     {
         public IWeapon _wpn { get; set; }
-        public abstract void Shoot(GameTime gametime, Vector2 position);
+        public abstract void Shoot(GameTime gameTime, Vector2 position);
     }
 }
