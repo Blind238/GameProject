08921b5ec778c65a1c5d07c36f6c772d9f7384fe
Jeremy Granadillo <akakitsu@gmail.com>
2013-04-04 13:59:33 +0200

    Laser placement finished, fixed damage

--- GameProject/GameProject/Weapons/LaserDecorator.cs
+++ GameProject/GameProject/Weapons/LaserDecorator.cs
@@ -15,15 +15,15 @@ namespace GameProject
         private static int _pelletNum = 10;
 
         private static float _totalDamage = 2.0f;
-        private static float _pelletDamage = _totalDamage / _pelletNum;
+        private static float _pelletDamage = _totalDamage / (float)_pelletNum;
 
         private static double _lastFired = 0;
         private static int _shootTimer = 500;
 
         public LaserDecorator(IWeapon wpn) {
             _scale = GameLogic.GetScale();
-            _offset = new Vector2(_scale, -(_scale*5));
-            _speed = new Vector2(0, -(_scale*2));
+            _offset = new Vector2(_scale*2.5f, -(_scale*5f));
+            _speed = new Vector2(0, -(_scale*4f));
             _wpn = wpn;
         }
 
@@ -32,21 +32,26 @@ namespace GameProject
             if (GameHelper.AllowedToFire(_lastFired, _shootTimer, gameTime))
             {
                 _lastFired = gameTime.TotalGameTime.TotalMilliseconds;
-                for (int i = 0; i < _pelletNum; i++)
+                for(int i = 0;i < 2;i++)
                 {
-                    Vector2 totalOffset = _offset;
-                    Vector2 verticalOffset = new Vector2(0, _scale);
-
-                    Projectile projectile = new Projectile(GameLogic.GetGame(), _pelletDamage);
-                    projectile.SetMovingBehaviour(new StraightLine(projectile, _speed));
-                    for (int j = 0; j < i; j++)
+                    for(int j = 0;j < _pelletNum;j++)
                     {
-                        totalOffset += verticalOffset;
+                        Vector2 totalOffset = _offset;
+                        Vector2 verticalOffset = new Vector2(0, _scale);
+
+                        Projectile projectile = new Projectile(GameLogic.GetGame(), _pelletDamage);
+                        projectile.SetMovingBehaviour(new StraightLine(projectile, _speed));
+                        for(int k = 0;k < j;k++)
+                        {
+                            totalOffset += verticalOffset;
+                        }
+                        projectile.SetPosition(position + totalOffset);
+                        GameLogic.GetGame().Components.Add(projectile);
+                        GameLogic.AddPlayerProjectile(projectile);
                     }
-                    projectile.SetPosition(position + totalOffset);
-                    GameLogic.GetGame().Components.Add(projectile);
-                    GameLogic.AddPlayerProjectile(projectile);
-                } 
+
+                    _offset = new Vector2(-_offset.X, _offset.Y);
+                }
             }
             _wpn.Shoot(gameTime, position);
         }
