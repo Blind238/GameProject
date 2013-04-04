60ab279025023fdf8b0895f9cae92e6e4492a322
Jeremy Granadillo <akakitsu@gmail.com>
2013-04-05 00:46:43 +0200

    Rocket complete

--- GameProject/GameProject/Weapons/RocketDecorator.cs
+++ GameProject/GameProject/Weapons/RocketDecorator.cs
@@ -8,24 +8,66 @@ namespace GameProject
 {
     public class RocketDecorator : WeaponUpgrade
     {
-        private Vector2 _offset = new Vector2(0, -25);
-        private Vector2 _speed = new Vector2(0, -10);
+        private Vector2 _offset;
+        private Vector2 _speed;
+        private float _scale;
+        private static float _rocketScale = 2.0f;
+        private enum SideFired { Left, Right }
+        private SideFired _sideFiredLast;
 
         private static int _pelletNum = 2;
 
         private static float _totalDamage = 3.0f;
-        private static float _pelletDamage = _totalDamage / _pelletNum;
+        private static float _pelletDamage = _totalDamage / (float)_pelletNum;
 
-        private static int _lastFired;
+        private static double _lastFired = 0;
         private static int _shootTimer = 1000;
 
         public RocketDecorator(IWeapon wpn) {
+            _scale = GameLogic.GetScale();
+            _offset = new Vector2(_scale*5f, -(_scale*0.5f));
+            _speed = new Vector2(0, -(_scale*2f));
             _wpn = wpn;
         }
 
         public override void Shoot(GameTime gameTime, Vector2 position)
         {
-            throw new NotImplementedException();
+            if (GameHelper.AllowedToFire(_lastFired, _shootTimer, gameTime))
+            {
+                _lastFired = gameTime.TotalGameTime.TotalMilliseconds;
+
+                Vector2 offset;
+                if (_sideFiredLast == SideFired.Left)
+                {
+                    offset = new Vector2(_offset.X, _offset.Y);
+                    _sideFiredLast = SideFired.Right;
+                }
+                else
+                {
+                    offset = new Vector2(-_offset.X, _offset.Y);
+                    _sideFiredLast = SideFired.Left;
+                }
+
+                for(int i = 0;i < _pelletNum;i++)
+                {
+                    Vector2 totalOffset = _offset;
+                    Vector2 verticalOffset = new Vector2(0, _scale * _rocketScale);
+
+                    Projectile projectile = new Projectile(GameLogic.GetGame(), _pelletDamage);
+                    projectile.SetMovingBehaviour(new StraightLine(projectile, _speed));
+                    for(int k = 0;k < i;k++)
+                    {
+                        totalOffset += verticalOffset;
+                    }
+                    projectile.SetPosition(position + totalOffset);
+                    projectile.SetScale(_scale * _rocketScale);
+                    GameLogic.GetGame().Components.Add(projectile);
+                    GameLogic.AddPlayerProjectile(projectile);
+                }
+
+                _offset = new Vector2(-_offset.X, _offset.Y);
+               
+            }
             _wpn.Shoot(gameTime, position);
         }
     }
