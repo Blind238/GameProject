bf9adb164f103203f2517e8135798f50fe88944a
Jeremy Granadillo <akakitsu@gmail.com>
2013-04-12 13:26:08 +0200

    Sinode and Wave movement are complete

--- GameProject/GameProject/Enemies/Sinode.cs
+++ GameProject/GameProject/Enemies/Sinode.cs
@@ -8,9 +8,36 @@ namespace GameProject
 {
     public class Sinode : Enemy
     {
+        private Random _random = new Random();
+
         public Sinode(Game game)
-            : base(game){
-            throw new System.NotImplementedException();
+            : base(game)
+        {
+            SetTexture(Resources.sinode);
+
+            // Spawn at one of the top corners
+            Vector2 startingPosition;
+            if (_random.Next(2) == 0)
+            {
+                startingPosition = new Vector2((float)game.GraphicsDevice.Viewport.Width * 0.2f, Bounds().Height);
+            }
+            else
+            {
+                startingPosition = new Vector2((float)game.GraphicsDevice.Viewport.Width * 0.8f, Bounds().Height);
+            }
+            SetPosition(startingPosition);
+
+            // Just using the first playerShip for aiming
+            MovingObject playerShip = (MovingObject)GameLogic.GetInstance().GetPlayerShips()[0];
+            Vector2 speed = GameHelper.PointToTarget(this, playerShip) * new Vector2(3.0f, 3.0f);
+            SetMovingBehaviour(new Wave(this, speed));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Wave wave = (Wave)GetMovingBehaviour();
+            wave.Move(gameTime);
+            base.Update(gameTime);
         }
     }
 }
---
+++ GameProject/GameProject/Movement/Wave.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameProject
+{
+    public class Wave : MovingBehaviour
+    {
+        private static double _amplitude = 10.0;
+        private static double _frequency = 1;
+        private Vector2 _direction;
+        private Vector2 _modDirection;
+        private float _speed;
+        private Vector2 _basePosition;
+        private double _startTime;
+        private double _elapsedTime;
+        private bool _resetTime = false;
+
+        private double _sinePosition = 0.0f;
+        private double _sineAngle;
+
+        /// <summary>
+        /// Sets forth a sine movement of 'movingObject'
+        /// along vector 'direction'
+        /// </summary>
+        /// <param name="movingObject"></param>
+        /// <param name="direction"></param>
+        public Wave(MovingObject movingObject, Vector2 direction)
+            : base(movingObject) 
+        {
+            _direction = direction;
+            _modDirection = new Vector2(-_direction.X, _direction.Y);
+            _speed = _direction.Length();
+            _basePosition = GetMovingObject().GetPosition();
+        }
+
+        public override void Move()
+        {
+            // Do nothing.
+        }
+
+        public void Move(GameTime gameTime) {
+            MovingObject movingObject = GetMovingObject();
+            Vector2 position = movingObject.GetPosition();
+            Viewport viewport = GameLogic.GetInstance().GetGame().GraphicsDevice.Viewport;
+
+            if (_startTime == 0.0 || _resetTime == true)
+            {
+                _startTime = gameTime.TotalGameTime.TotalMilliseconds;
+                _resetTime = false;
+            }
+            
+            _elapsedTime = gameTime.TotalGameTime.TotalMilliseconds - _startTime;
+
+            Vector2 v = _modDirection;
+            v.Normalize();
+            v = v *
+                (float)Math.Sin(
+                    _elapsedTime * Math.PI * 0.002
+                    ) *
+                (float)_amplitude;
+            v *= _speed;
+            v = v + _basePosition;
+            _basePosition += _direction;
+            movingObject.SetPosition(v);
+
+            position = movingObject.GetPosition();
+            if (position.X > viewport.Width - (movingObject.Bounds().Width / 2))
+            {
+                movingObject.SetPosition(new Vector2((viewport.Width - (movingObject.Bounds().Width / 2)), position.Y));
+                _direction = _direction * new Vector2(-1, 1);
+                _modDirection = new Vector2(_direction.X, -_direction.Y);
+                _basePosition = movingObject.GetPosition();
+                _startTime = gameTime.TotalGameTime.TotalMilliseconds;
+                //_resetTime = true;
+            }
+            if (position.X < movingObject.Bounds().Width / 2)
+            {
+                movingObject.SetPosition(new Vector2((movingObject.Bounds().Width / 2), position.Y));
+                _direction = _direction * new Vector2(-1, 1);
+                _modDirection = new Vector2(_direction.X, -_direction.Y);
+                _basePosition = movingObject.GetPosition();
+                _startTime = gameTime.TotalGameTime.TotalMilliseconds;
+                //_resetTime = true;
+            }
+            if (position.Y > viewport.Height - (movingObject.Bounds().Height / 2))
+            {
+                movingObject.SetPosition(new Vector2(position.X, (viewport.Height - (movingObject.Bounds().Height / 2))));
+                _direction = _direction * new Vector2(1, -1);
+                _modDirection = new Vector2(-_direction.X, _direction.Y);
+                _basePosition = movingObject.GetPosition();
+                _startTime = gameTime.TotalGameTime.TotalMilliseconds;
+
+                //_resetTime = true;
+            }
+            if (position.Y < movingObject.Bounds().Height / 2)
+            {
+                movingObject.SetPosition(new Vector2(position.X, (movingObject.Bounds().Height / 2)));
+                _direction = _direction * new Vector2(1, -1);
+                _modDirection = new Vector2(-_direction.X, _direction.Y);
+                _basePosition = movingObject.GetPosition();
+                _startTime = gameTime.TotalGameTime.TotalMilliseconds;
+                //_resetTime = true;
+            }
+        }
+    }
+}
